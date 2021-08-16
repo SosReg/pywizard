0cf5ca9cf0ff772753b4dd7dab4303c2901f351e
SosReg <43793025+SosReg@users.noreply.github.com>
2021-08-16 10:38:43 -0500

    Update settings.py

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b)1di3^2%#2##))yo6@raki#ao#0q879o=y5nvxmddz*vf(*j7'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['104.248.219.71','localhost','www.pywizard.com','pywizard.com', '127.0.0.1', '127.0.0.1:8000']
 
